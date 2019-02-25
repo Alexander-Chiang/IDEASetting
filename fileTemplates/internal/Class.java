c9a7e7c510fd3df61ab3d533ca95cf7e99a33f3f
jiangyayu <jiangyayu@cmhi.chinamobile.com>
2019-02-25 12:19:33 +0800

    IU-2018.3.4 <Yayu.Jiang@DESKTOP-V681VJ1 Create baseRefactoring.xml, markdown.xml, ide.general.xml, _@user_Default.icls, Constructor JavaDoc.tpl, project.default.xml, Interface.java, other.xml, Default.xml, Parameter JavaDoc.tpl, find.xml, ui.lnf.xml, Get Method JavaDoc.tpl, filetypes.xml, checkstyle-idea.xml, colors.scheme.xml, Solarized Dark..icls, notifications.xml, VisualVMLauncher.xml, databaseDrivers.xml, Interface JavaDoc.tpl, templates.xml, Tamacun.icls, Set Method JavaDoc.tpl, Default.xml, laf.xml, Class.java, jdk.table.xml, Class JavaDoc.tpl, path.macros.xml, Method JavaDoc.tpl, MyBatisCodeHelper.xml, User.xml, databaseSettings.xml, Throws clause JavaDoc.tpl, Return type JavaDoc.tpl, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+ 
+/** 
+ * <br/>
+ * @author ${USER}
+ * @date ${DATE} ${TIME}
+ * @history 
+ * 		  ${DATE} ${TIME} ${USER} create.
+ */
+public class ${NAME} {
+}
